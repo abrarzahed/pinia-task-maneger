3cac1ea87ad19d9158ddc8125e12fe5954c12859
abrarzahed <abrarzahed6986@gmail.com>
2022-11-09 11:19:28 +0600

    added icon font and pinia

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <!-- header -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="" />
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="(task, i) in taskStore.tasks" :key="i">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { useTaskStore } from "./stores/TaskStore";
+import TaskDetails from "./components/TaskDetails.vue";
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
+};
+</script>
